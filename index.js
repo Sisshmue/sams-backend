6fc223eaadbb06deefbfb8743b5b346d9563e7b4
sisshmue <sisshmue404@gmail.com>
2026-08-17 16:52:00 +0700

    Register new routes and update HTTP tests

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import userRoute from "./src/routes/user.route.js";
 import authRoute from "./src/routes/auth.route.js";
 import assetRoute from "./src/routes/asset.route.js";
 import assignmentRoute from "./src/routes/assignment.route.js";
+import maintenanceRoute from "./src/routes/maintenance.route.js";
+import departmentRoute from "./src/routes/department.route.js";
+import activityLogRoute from "./src/routes/activityLog.route.js";
 
 const app = express();
 
@@ -13,6 +16,9 @@ app.use("/auth", authRoute);
 app.use("/user", userRoute);
 app.use("/asset", assetRoute);
 app.use("/assignment", assignmentRoute);
+app.use("/maintenance", maintenanceRoute);
+app.use("/department", departmentRoute);
+app.use("/activity-log", activityLogRoute);
 
 const port = 3000;
 
